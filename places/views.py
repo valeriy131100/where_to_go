1660d3b05e5e9eb45a8863f130f0f9331260e9d8
Valery Efremov <valeriy131100@gmail.com>
2022-02-03 14:51:41 +0300

    Add frontend

--- places/views.py
+++ places/views.py
@@ -1,17 +1,5 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse(
-        """
-        <!DOCTYPE html>
-        <html>
-        <head>
-          <title>Стартовая</title>
-        </head>
-        <body>
-          <h1>Здесь будет карта</h1>
-        </body>
-        </html>
-        """
-    )
+    return render(request, 'index.html')
