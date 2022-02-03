b116f8f77152dcb426013983bd3ea584d4d07f41
Valery Efremov <valeriy131100@gmail.com>
2022-02-03 14:20:34 +0300

    Add blank start page

--- places/views.py
+++ places/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse(
+        """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Стартовая</title>
+        </head>
+        <body>
+          <h1>Здесь будет карта</h1>
+        </body>
+        </html>
+        """
+    )
