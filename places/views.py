eaf0b9751f938d2a6dd80dfc21cf90b8154a3328
Valery Efremov <valeriy131100@gmail.com>
2022-02-04 10:29:18 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
 
+from .models import Place
+
+
+def serialize_place(place: Place):
+    return {
+        'type': 'Feature',
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                place.longitude,
+                place.latitude
+            ],
+        },
+        'properties': {
+            'title': place.title,
+            'placeId': place.id,
+            # replace this
+            'detailsUrl': './static/places/moscow_legends.json'
+        }
+
+    }
+
 
 def index(request):
-    return render(request, 'index.html')
+    places = Place.objects.prefetch_related('images')
+
+    places_context = {
+        'type': 'FeatureCollection',
+        'features': [
+            serialize_place(place) for place in places
+        ]
+    }
+
+    return render(request, 'index.html', {'places': places_context})
