5bdd9506e72126bedd574f1f42a5962be7f6ff75
Valery Efremov <valeriy131100@gmail.com>
2022-02-07 10:17:24 +0300

    Remove some env settings

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -61,8 +61,7 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 DATABASES = {
     'default': env.dj_db_url(
         'DATABASE_URL',
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        ssl_require=not DEBUG
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
     )
 }
 
@@ -83,9 +82,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = env.str('LANGUAGE_CODE', default='ru-ru')
 TIME_ZONE = env.str('TIME_ZONE', default='UTC')
-USE_I18N = env.bool('USE_I18N', default=True)
-USE_L10N = env.bool('USE_L10N', default=True)
-USE_TZ = env.bool('USE_TZ', default=True)
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
 
 STATIC_URL = env.str('STATIC_URL', default='/static/')
 STATIC_ROOT = env.str('STATIC_ROOT', default=str(BASE_DIR / 'static'))
