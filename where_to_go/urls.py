ccaf54e814a8a4922c6e487b0662b528936e3121
Valery Efremov <valeriy131100@gmail.com>
2022-02-07 15:24:22 +0300

    Connect media to urlpatterns only if DEBUG

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('', views.index),
     path('places/<int:place_id>', views.get_place_by_id),
     path('tinymce/', include('tinymce.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
